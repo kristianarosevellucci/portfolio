58d7664462319800a7291fa4832c91c9b3a11980
kristianarosevellucci <me@kristianavellucci.com>
2017-03-20 17:30:18 +0000

    Live Updates

--- contact.php
+++ contact.php
@@ -1,43 +1,53 @@
 <?php
 
-if (isset($_POST["submit"])) {
+if (isset($_POST["name"])) {
   $name = $_POST['name'];
   $subject = $_POST['subject'];
   $email = $_POST['email'];
   $message = $_POST['message'];
-  $human = intval($_POST['human']);
+  $human = empty($_POST['human']);
+
+
   $from = 'Kristianavellucci.com Contact Form';
-  $to = 'hello@kvellucci.com';
+  $to = 'me@kristianavellucci.com';
   $subject = 'Message from Portfolio Website ';
+
+$headers = 'From: me@kristianavellucci.com' . "\r\n" .
+    'Reply-To: me@kristianavellucci.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
   $body = "From: $name\n Subject: $subject\n E-Mail: $email\n Message:\n $message";
 
+
   // Check if name has been entered
-  if (!$_POST['name']) {
+  if (empty($_POST['name'])) {
   	$errName = 'Please enter your name';
   }
 
   // Check if email has been entered and is valid
-  if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   	$errEmail = 'Please enter a valid email address';
   }
 
   //Check if message has been entered
-  if (!$_POST['message']) {
+  if (empty($_POST['message'])) {
   	$errMessage = 'Please enter your message';
   }
   //Check if simple anti-bot test is correct
-  if ($human !== 5) {
+  if (!$human) {
   	$errHuman = 'Your anti-spam is incorrect';
   }
 
   // If there are no errors, send the email
   if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
-  	if (mail ($to, $subject, $body, $from)) {
+  	if (mail ($to, $subject, $body, $headers)) {
       header('Location: thank-you.php');
   	} else {
   		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
   	}
-  }
+   } else {
+	print "Parameters incorrect";
+   }
+} else {
+	print "Noooo";
 }
-?>
--- test.php
+++ test.php
@@ -5,7 +5,12 @@
     $to = "me@kristianavellucci.com";
     $subject = "PHP Mail Test script";
     $message = "This is a test to check the PHP Mail functionality";
-    $headers = "From:" . $from;
+//    $headers = "From:" . $from;
+    
+$headers = 'From: me@kristianavellucci.com' . "\r\n" .
+    'Reply-To: me@kristianavellucci.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+
     mail($to,$subject,$message, $headers);
     echo "Test email sent";
-?>
